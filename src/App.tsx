1bf966d0b6fb25aeedfed25008a68655325e619c
SoJuSo <asdfg7123@gmail.com>
2024-07-01 23:58:54 +0900

    ✨feat : Implement simple routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
+import { useEffect, useState } from "react";
+import ScrollPhoneNumberPage from "./pages/ScrollPhoneNumberPage";
+import GivenNumberCalculatorPage from "./pages/GivenNumberCalculatorPage";
+import RandomNumberCalculatorPage from "./pages/RandomNumberCalculatorPage";
+import NotFound from "./pages/NotFoundPage";
+import { routeList } from "./constants/route";
+
 const App = () => {
-  return (
-    <>
-      <div></div>
-    </>
-  );
+  const [pageName, setPageName] = useState("");
+  useEffect(() => {
+    setPageName(window.location.pathname);
+  }, []);
+
+  // Todo GNB
+
+  switch (pageName) {
+    case routeList.HOME:
+      return <div>Home</div>;
+    case routeList.ScrollPhoneNumber:
+      return <ScrollPhoneNumberPage />;
+    case routeList.GivenNumberCalculator:
+      return <GivenNumberCalculatorPage />;
+    case routeList.RandomNumberCalculator:
+      return <RandomNumberCalculatorPage />;
+    default:
+      return <NotFound />;
+  }
 };
 
 export default App;
