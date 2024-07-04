79055f6cce725f354bc35f73a0ff1984283c2863
SoJuSo <asdfg7123@gmail.com>
2024-07-04 17:58:58 +0900

    🎨style : Create themeProvier

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { GlobalStyle } from "./styles/globalStyles.ts";
+import { ThemeProvider } from "styled-components";
+import theme from "./styles/theme.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
---
+++ src/styles/theme.ts
@@ -0,0 +1,16 @@
+const lightTheme = {
+  backgroundColor: "#eee",
+  textColor: "#999",
+};
+
+const darkTheme = {
+  backgroundColor: "#999",
+  textColor: "#eee",
+};
+
+const Theme = {
+  lightTheme,
+  darkTheme,
+};
+
+export default Theme;
