260cf25ccf83ec4ec0d83e97e966b54c96901644
SoJuSo <asdfg7123@gmail.com>
2024-07-15 02:21:30 +0900

    ✨feat : Rendering layout elements for background based on width

---
+++ src/Layout.tsx
@@ -0,0 +1,19 @@
+import BackgroundPhone from "./components/BackgroundIcon/BackgroundPhone";
+import useDebounceWidth from "./hooks/useDebounceWidth";
+import { Area, Circles } from "./styles/BackgroundLayout";
+
+const Layout = () => {
+  const windowWidth = Math.max(Math.trunc(useDebounceWidth() / 100), 7);
+  const currentWidth = windowWidth < 20 ? windowWidth : 19;
+  return (
+    <Area>
+      <Circles>
+        {Array.from({ length: currentWidth * 2 }, (_, i) => (
+          <BackgroundPhone key={i} />
+        ))}
+      </Circles>
+    </Area>
+  );
+};
+
+export default Layout;
