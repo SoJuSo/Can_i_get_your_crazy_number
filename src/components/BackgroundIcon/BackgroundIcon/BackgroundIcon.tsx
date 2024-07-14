a06ba4b7491276782cd95d334fcc352a2fabab32
SoJuSo <asdfg7123@gmail.com>
2024-07-15 02:22:18 +0900

    ✨feat : Implementing components for the background
    - Icon component to render background icons
    - BackgroundPhone component to provide random data

---
+++ src/components/BackgroundIcon/BackgroundIcon/BackgroundIcon.tsx
@@ -0,0 +1,43 @@
+// components/BackgroundIcon.tsx
+import { GiRotaryPhone } from "react-icons/gi";
+import { IoIosPhonePortrait } from "react-icons/io";
+import { TiPhone } from "react-icons/ti";
+import { GiSmartphone } from "react-icons/gi";
+import { PiPhoneCallLight } from "react-icons/pi";
+
+import { selectRandomColors } from "../../../utils/random";
+
+interface BackgroundIconProps {
+  random: number;
+  size?: number;
+}
+
+const BackgroundIcon = ({ random = 1, size = 70 }: BackgroundIconProps): JSX.Element => {
+  const [startColor, endColor] = selectRandomColors();
+  console.log(startColor, endColor);
+
+  const Icon =
+    random === 1
+      ? IoIosPhonePortrait
+      : random === 2
+        ? GiRotaryPhone
+        : random === 3
+          ? TiPhone
+          : random === 4
+            ? GiSmartphone
+            : PiPhoneCallLight;
+
+  return (
+    <li>
+      <Icon size={size} style={{ fill: `url(#icon-gradient${random + size})` }} />
+      <svg width="0" height="0">
+        <linearGradient id={`icon-gradient${random + size}`} x1="100%" y1="100%" x2="0%" y2="0%">
+          <stop stopColor={startColor} offset="25%" />
+          <stop stopColor={endColor} offset="75%" />
+        </linearGradient>
+      </svg>
+    </li>
+  );
+};
+
+export default BackgroundIcon;
