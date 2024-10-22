99127964329b7fd196983f6715052e51fcdcb8da
SoJuSo <asdfg7123@gmail.com>
2024-10-22 17:55:05 +0900

    ♻️refactor : refactoring PhoneNumberInput logic

--- src/components/ScrollPhoneNumber/PhoneNumberInput/PhoneNumberInput.tsx
+++ src/components/ScrollPhoneNumber/PhoneNumberInput/PhoneNumberInput.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { styled } from "styled-components";
+
 interface PhoneNumberInputProps {
   phoneNumber: number;
   setPhoneNumber: (value: number) => void;
@@ -9,44 +10,63 @@ interface PhoneNumberInputProps {
   level: "easy" | "normal" | "hard" | "korean";
 }
 
-const PhoneNumberInput: React.FC<PhoneNumberInputProps> = ({
+const StyledInput = styled.input<{ level: string }>`
+  width: ${({ level }) =>
+    level === "easy"
+      ? "300px"
+      : level === "korean"
+        ? "50px"
+        : level === "hard"
+          ? "100px"
+          : "200px"};
+`;
+
+const PhoneNumberInput = ({
   phoneNumber,
   setPhoneNumber,
   activeWheel,
   activeKeyBoard,
   level,
-}) => {
+}: PhoneNumberInputProps) => {
+  const handleWheel = (e: React.WheelEvent<HTMLInputElement>) => {
+    if (activeWheel) {
+      if (e.deltaY < 0 && phoneNumber < 99999999) {
+        setPhoneNumber(phoneNumber + 1);
+      } else if (e.deltaY > 0 && phoneNumber > 0) {
+        setPhoneNumber(phoneNumber - 1);
+      }
+    }
+  };
+
   return (
-    <>
-      <input
-        onKeyDown={(e) => {
-          if (!activeKeyBoard) {
-            e.preventDefault();
-          }
-        }}
-        style={{
-          width: level === "easy" ? "300px" : level === "korean" ? "50px" : level === "hard" ? "100px" : "200px",
-        }}
-        type="range"
-        min={0}
-        max={99999999}
-        step={1}
-        value={phoneNumber}
-        tabIndex={0}
-        onChange={(e) => {
-          setPhoneNumber(e.target.valueAsNumber);
-        }}
-        onWheel={(e) => {
-          if (activeWheel) {
-            if (e.deltaY < 0 && phoneNumber < 99999999) {
-              setPhoneNumber(phoneNumber + 1);
-            } else if (e.deltaY > 0 && phoneNumber > 0) {
-              setPhoneNumber(phoneNumber - 1);
-            }
-          }
-        }}
-      />
-    </>
+    <StyledInput
+      onKeyDown={(e) => {
+        if (!activeKeyBoard) {
+          e.preventDefault();
+        }
+      }}
+      style={{
+        width:
+          level === "easy"
+            ? "300px"
+            : level === "korean"
+              ? "50px"
+              : level === "hard"
+                ? "100px"
+                : "200px",
+      }}
+      type="range"
+      min={0}
+      max={99999999}
+      step={1}
+      value={phoneNumber}
+      tabIndex={0}
+      onChange={(e) => {
+        setPhoneNumber(e.target.valueAsNumber);
+      }}
+      onWheel={handleWheel}
+      level={level}
+    />
   );
 };
 
